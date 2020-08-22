5ce1d567d75bbb79aeb7c1a0c04a0b4ca25338d0
Dat Tran <trduydat@gmail.com>
2020-08-22 19:32:57 +0700

    Init project

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+FONTS_DIR=$HOME/.fonts
+LIST_FONTS=("CascadiaCodePL.ttf" "FiraCode-SemiBold.ttf" "Hasklig-ExtraLightIt.otf" "Hasklig-Semibold.otf" "InputMono-Light.ttf" "SourceCodePro-BoldIt.ttf" "SourceCodePro-Regular.ttf" "CascadiaCode.ttf" "Hack-BoldItalic.ttf" "Hasklig-ExtraLight.otf" "InputMono-BlackItalic.ttf" "InputMono-MediumItalic.ttf" "SourceCodePro-Bold.ttf" "SourceCodePro-SemiboldIt.ttf" "CascadiaMonoPL.ttf" "Hack-Bold.ttf" "Hasklig-It.otf" "InputMono-Black.ttf" "InputMono-Medium.ttf" "SourceCodePro-ExtraLightIt.ttf  SourceCodePro-Semibold.ttf" "CascadiaMono.ttf" "Hack-Italic.ttf" "Hasklig-LightIt.otf" "InputMono-BoldItalic.ttf" "InputMono-Regular.ttf" "SourceCodePro-ExtraLight.ttf" "FiraCode-Bold.ttf" "Hack-Regular.ttf" "Hasklig-Light.otf" "InputMono-Bold.ttf" "InputMono-ThinItalic.ttf" "SourceCodePro-It.ttf" "FiraCode-Light.ttf" "Hasklig-BlackIt.otf" "Hasklig-MediumIt.otf" "InputMono-ExtraLightItalic.ttf" "InputMono-Thin.ttf" "SourceCodePro-LightIt.ttf" "FiraCode-Medium.ttf" "Hasklig-Black.otf" "Hasklig-Medium.otf" "InputMono-ExtraLight.ttf" "monaco.ttf" "SourceCodePro-Light.ttf" "FiraCode-Regular.ttf" "Hasklig-BoldIt.otf" "Hasklig-Regular.otf" "InputMono-Italic.ttf" "SourceCodePro-BlackIt.ttf" "SourceCodePro-MediumIt.ttf" "FiraCode-Retina.ttf" "Hasklig-Bold.otf" "Hasklig-SemiboldIt.otf" "InputMono-LightItalic.ttf" "SourceCodePro-Black.ttf" "SourceCodePro-Medium.ttf")
+
+function die() {
+	echo "${@}"
+	exit 1
+}
+
+cat << EOF
+--------------------------------
+Easy Programming Fonts Installer
+--------------------------------
+- Maintained by Dat Tran
+
+EOF
+
+# Create font dir if not exists
+if [[ ! -e $FONTS_DIR ]]; then
+	mkdir $FONTS_DIR || die "Could not make $FONTS_DIR"
+fi
+
+for FONT in ${FONTS[*]}; do
+	echo "Downloading $FONT"; echo
+	wget -c https://github.com/trdat/easy-programming-fonts-installer/raw/master/$FONT \
+	|| die "Failed to download ${FONT}"
+	mv $FONT $FONTS_DIR || die "Could not install $FONT"
+	echo "Installed $FONT successfully"; echo
+done
+
+cat << EOF
+--------------------
+All fonts installed!
+--------------------
+EOF
